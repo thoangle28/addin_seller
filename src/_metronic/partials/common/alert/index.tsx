8c37cd389f0d4e4aa258da1cbaece2bf54dde917
Quang.Fe.Dev <voquang1406@gmail.com>
2022-03-31 17:26:18 +0700

    optimize component

--- src/_metronic/partials/common/alert/index.tsx
+++ src/_metronic/partials/common/alert/index.tsx
@@ -1,10 +1,12 @@
 import './style.scss'
 interface IAlertProps {
-    alertClass: string;
+    hasErrors: boolean;
     message: string;
 }
 
-const AlertMessage = ({ alertClass, message }: IAlertProps) => {
+const AlertMessage = ({ hasErrors, message }: IAlertProps) => {
+    const alertClass = `${hasErrors ? 'mb-lg-15 alert alert-danger' : 'mb-lg-8 p-8 alert-success'}`
+
     return <div className={alertClass}>
         <div className='alert-text font-weight-bold'>
             <p className='upper-st-letter text-default text-center mb-0'>{message}</p>
