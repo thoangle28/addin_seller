6062bbb08fabec372c6cca71c5ea3a19a050d349
Quang.Fe.Dev <voquang1406@gmail.com>
2022-04-06 09:08:05 +0700

    hot fix feedbacks

--- src/app/modules/products/components/Attribute.tsx
+++ src/app/modules/products/components/Attribute.tsx
@@ -1,11 +1,12 @@
 import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { FC, useEffect, useState } from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 import * as Yup from 'yup'
+import { RootState } from '../../../../setup';
 import AlertMessage from '../../../../_metronic/partials/common/alert';
-import { getAttributes, updateAttr } from '../redux/ProductsList';
+import { getAttributesById, updateAttr } from '../redux/ProductsList';
 
 interface Values {
-    attribute: string;
     old_attribute_name: string;
     new_attribute_name: string;
 
@@ -15,30 +16,33 @@ const Attribute: FC = () => {
     const [message, setMessage] = useState<string>()
     const [hasErrors, setHasErrors] = useState<boolean>(true)
 
+    const user: any = useSelector<RootState>(({ auth }) => auth.user, shallowEqual)
+    const currentUserId = user ? user.ID : 0
+
     const initialValues = {
-        attribute: '',
         old_attribute_name: '',
         new_attribute_name: ''
     }
 
     const validateSchema = Yup.object().shape({
-        old_attribute_name: Yup.string().required('Old Attribute Must Be Required!').oneOf([Yup.ref('attribute'), null], 'Attribute have to match'),
-        new_attribute_name: Yup.string().required('New Attribute Must Be Required!')
+        old_attribute_name: Yup.string().required('Old Attribute is Required!'),
+        new_attribute_name: Yup.string().required('New Attribute is Required!')
     })
 
     const fetchData = () => {
-        getAttributes().then((res: any) => {
+        getAttributesById(currentUserId).then((res: any) => {
             setParentAttribute(res.data.data)
         }).catch(err => console.log(err));
     }
 
-    const updateDataAttr = (old_attribute_name: string, new_attribute_name: string) => {
+    const updateDataAttr = (old_attribute_name: string, new_attribute_name: string, resetForm: any) => {
         updateAttr(old_attribute_name, new_attribute_name).then(res => {
             const { code, message } = res.data
             setMessage('processing')
             if (code === 200) {
                 setHasErrors(false)
                 setMessage(message)
+                resetForm()
                 setTimeout(() => {
                     setMessage('')
                 }, 3000);
@@ -51,7 +55,7 @@ const Attribute: FC = () => {
 
     useEffect(() => {
         fetchData();
-    }, [parentAttribute])
+    }, [])
 
 
     const showData = () => !!parentAttribute && parentAttribute.map((attr: any) => <option key={attr.id}>{attr.label}</option>)
@@ -60,24 +64,7 @@ const Attribute: FC = () => {
 
         return (
             <div className="form-group mt-4 row">
-                <div className='col-md-6 mb-5'>
-                    <label htmlFor='old_attribute_name' className='d-flex align-items-center fs-7 fw-bold mb-2'>
-                        <span>Old Attribute</span>
-                    </label>
-                    <Field
-                        component="input"
-                        id="old_attribute_name"
-                        type='text'
-                        className='form-control fs-7'
-                        name='old_attribute_name'
-                        data-bs-toggle='tooltip'
-                        data-bs-placement='top'
-                    />
-                    {touched.old_attribute_name && errors.old_attribute_name && (
-                        <div className='text-danger mt-2'>{errors.old_attribute_name}</div>
-                    )}
-                </div>
-                <div className='col-md-6 mb-5'>
+                <div className='col-md-12 mb-5'>
                     <label htmlFor='new_attribute_name' className='d-flex align-items-center fs-7 fw-bold mb-2'>
                         <span>New Attribute</span>
                     </label>
@@ -110,22 +97,21 @@ const Attribute: FC = () => {
                 <Formik
                     initialValues={initialValues}
                     validationSchema={validateSchema}
-                    onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
-                        console.log(values)
+                    onSubmit={(values: Values, { resetForm }: FormikHelpers<Values>,) => {
                         const { old_attribute_name, new_attribute_name } = values
-                        updateDataAttr(old_attribute_name, new_attribute_name)
+                        updateDataAttr(old_attribute_name, new_attribute_name, resetForm)
                     }}
                 >
                     {({ errors, touched }) => (
                         <Form>
 
                             {message && <AlertMessage hasErrors={hasErrors} message={message} />}
-                            <label htmlFor="attribute">Select Parent Attributes</label>
+                            <label htmlFor="old_attribute_name">Select Parent Attributes</label>
                             <Field
                                 component="select"
                                 as="select"
-                                id="attribute"
-                                name="attribute"
+                                id="old_attribute_name"
+                                name="old_attribute_name"
                                 className="form-select"
                             >
                                 <option value="">None</option>
