64b4fe7cc50c1da24eb09819686bba48e4d16fad
thoangle28 <85162410+thoangle28@users.noreply.github.com>
2022-05-25 15:58:29 +0700

    Update Fix Attributes

--- src/app/modules/products/components/ProductCreate.tsx
+++ src/app/modules/products/components/ProductCreate.tsx
@@ -253,10 +253,21 @@ const ProductCreate: FC<PropsFromRedux> = (props) => {
   /** Add Variations */
   const createVariations = (numToAdd: number, maxAllow: number, formValues: any) => {
     const listAttr: any = [] 
-    formValues.variations_attr &&
-      formValues.variations_attr.map((e: any) => {
-        listAttr.push({ attr: e, id: 0, label: '', value: '' })
+    //load variations of product
+    /* formValues.variations_attr && formValues.variations_attr.map((e: any) => {
+      listAttr.push({ attr: e, id: 0, label: '', value: '' })
+    }) */
+    //if not 
+
+    formValues.attributes && formValues.attributes.map((e: any) => {
+      const checkExisted = formValues.variations.some((x: any) => {
+        return x.name === e.name
       })
+
+      if( e.variation && !checkExisted) {
+        listAttr.push({ attr: e.name, id: 0, label: '', value: '' })
+      }
+    })
 
     let nextVar = 0
 
@@ -558,7 +569,7 @@ const ProductCreate: FC<PropsFromRedux> = (props) => {
   const configCKEditor = {
     extraPlugins: [uploadPlugin],
   }
-
+  
   return (
     <>
       {loading && formStatus.error == 204 ? (
