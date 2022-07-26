ed03d711a55fc46562c91e71b0a159b032eb4aa8
Quang.Lotus.Dev <voquang1406@gmail.com>
2022-07-26 15:33:29 +0700

    hot fix

--- src/app/modules/products/components/OrderListingRedux/Actions.ts
+++ src/app/modules/products/components/OrderListingRedux/Actions.ts
@@ -1,40 +1,70 @@
-import { iPayload, iUpdateData } from '../../../../../models'
+import { iOrderListDetailResponse, iOrderListResponse, iPayload, iUpdateData } from '../../../../../models'
 import { actionTypes } from './Types'
 import { getOrderListPage, getOrderDetailById, updateOrderStatus } from './../../redux/ProductsList'
 
-const isSuccess = (type: string, payload?: any) => ({ type, payload });
-const isFailure = (type: string, payload?: any) => ({ type, payload })
-const isRequest = (type: string) => ({ type })
+const getAllOrderFailure = (payload: string) => ({
+    type: actionTypes.GET_ALL_ORDER_LISTING_FAILURE,
+    payload
+})
+const getAllOrderRequest = () => ({
+    type: actionTypes.GET_ALL_ORDER_LISTING_REQUEST
+})
+const getAllOrderListingSuccess = (payload: iOrderListResponse) => ({
+    type: actionTypes.GET_ALL_ORDER_LISTING_SUCCESS,
+    payload
+})
 
 export const fetchOrderListings = (iPayload: iPayload) => async (dispatch: any) => {
-    dispatch(isRequest(actionTypes.GET_ALL_ORDER_LISTING_REQUEST))
+    dispatch(getAllOrderRequest())
     try {
         const res = await getOrderListPage(iPayload);
         const { data, code, message } = res.data
-        code !== 200 ? dispatch(isFailure(actionTypes.GET_ALL_ORDER_LISTING_FAILURE, message)) : dispatch(isSuccess(actionTypes.GET_ALL_ORDER_LISTING_SUCCESS, data))
+        code !== 200 ? dispatch(getAllOrderFailure(message)) : dispatch(getAllOrderListingSuccess(data))
     } catch (error: any) {
-        dispatch(isFailure(actionTypes.GET_ALL_ORDER_LISTING_FAILURE, error.message))
+        dispatch(getAllOrderFailure(error.message))
     }
 }
+const updateOrderListingDetailFailure = () => ({
+    type: actionTypes.UPDATE_ORDER_LISTING_DETAILS_FAILURE
+})
+const updateOrderListingDetailRequest = () => ({
+    type: actionTypes.UPDATE_ORDER_LISTING_DETAILS_REQUEST
+})
+const updateOrderListingDetailSuccess = () => ({
+    type: actionTypes.UPDATE_ORDER_LISTING_DETAILS_SUCCESS
+})
 
 export const updateOrderListingDetail = (payload: iUpdateData) => async (dispatch: any) => {
-    dispatch(isRequest(actionTypes.UPDATE_ORDER_LISTING_DETAILS_REQUEST))
+    dispatch(updateOrderListingDetailRequest())
     try {
         const res = await updateOrderStatus(payload)
         const { code } = res.data
-        code !== 200 ? dispatch(isFailure(actionTypes.UPDATE_ORDER_LISTING_DETAILS_FAILURE)) : dispatch((actionTypes.UPDATE_ORDER_LISTING_DETAILS_SUCCESS))
+        code !== 200 ? dispatch(updateOrderListingDetailFailure()) : dispatch(updateOrderListingDetailSuccess())
     } catch (error) {
-        dispatch(isFailure(actionTypes.UPDATE_ORDER_LISTING_DETAILS_FAILURE))
+        dispatch(updateOrderListingDetailFailure())
     }
 }
 
+const getOrderDetailFailure = () => ({
+    type: actionTypes.GET_ORDER_LISTING_DETAILS_FAILURE
+})
+
+const getOrderDetailRequest = () => ({
+    type: actionTypes.GET_ORDER_LISTING_DETAILS_REQUEST
+})
+
+const getOrderDetailSuccess = (payload: iOrderListDetailResponse) => ({
+    type: actionTypes.GET_ORDER_LISTING_DETAILS_SUCCESS,
+    payload
+})
+
 export const getOrderListingById = (id: string | number, order_id: string, access_token: string) => async (dispatch: any) => {
-    dispatch(isRequest(actionTypes.GET_ORDER_LISTING_DETAILS_REQUEST))
+    dispatch(getOrderDetailRequest())
     try {
         const res = await getOrderDetailById(id, order_id, access_token);
         const { data, code } = res.data
-        code !== 200 ? dispatch(isFailure(actionTypes.GET_ORDER_LISTING_DETAILS_FAILURE)) : dispatch(isSuccess(actionTypes.GET_ORDER_LISTING_DETAILS_SUCCESS, data))
+        code !== 200 ? dispatch(getOrderDetailFailure()) : dispatch(getOrderDetailSuccess(data))
     } catch (error) {
-        dispatch(isFailure(actionTypes.GET_ORDER_LISTING_DETAILS_FAILURE))
+        dispatch(getOrderDetailFailure())
     }
 }
